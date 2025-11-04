8895f707a88a0591d599b5b2abba474a8f94337a
Medykrena <ari.data@proton.me>
2025-11-04 11:09:46 +0100

    Add CORS header to ping function response

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,8 +1,13 @@
 // netlify/functions/ping.js
+'use strict';
+
 exports.handler = async () => {
   return {
     statusCode: 200,
-    headers: { 'Content-Type': 'application/json; charset=utf-8' },
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({ ok: true, msg: 'functions alive' })
   };
 };
